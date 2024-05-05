1f8fed582deba04e235d21b886de5a26b736cbba
Trishala <54235749+tesla07@users.noreply.github.com>
2024-05-05 00:08:46 +0000

    explore and import

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-let num1 = process.argv[2];
-let num2 = process.argv[3];
-let num3 = process.argv[4];
-console.log(num1 + " "+num2 +" " +num3);
+const math = require("./math.js");
+let num1 = parseInt(process.argv[2]);
+let num2 = parseInt(process.argv[3]);
+let re1 = math.add(num1, num2);
+let re2 = math.sub(num1, num2);
+ console.log(`sum of numbers ${num1} and ${num2} is ${re1} and difference is ${re2}`);
